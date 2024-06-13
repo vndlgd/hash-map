7a27abde2aef97375f7bca5af245c1b22bba4187
Ivan Delgado <46804151+vndlgd@users.noreply.github.com>
2024-06-12 23:06:19 -0700

    Implement initial HashMap functionality

--- hashmap.js
+++ hashmap.js
@@ -1,51 +1,212 @@
 function HashMap() {
-  // TODO: implement all functions
+  let buckets = [];
+  let currentCapacity = 16; // total number of bucketes we currently have
+  buckets.length = currentCapacity; // initialize an array size
+  const loadFactor = 0.75;
+
   function hash(key) {
     let hashCode = 0;
 
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % buckets.length;
     }
 
     return hashCode;
   }
 
   function set(key, value) {
-    // write code here
+    let index = hash(key);
+    // workaround for JavaScript's dynamic nature of arrays
+    if (index < 0 || index >= buckets.length) {
+      throw new Error('Trying to access index out of bound');
+    }
+    // this handles entering new values and updating old values if index is or isn't found
+    let obj = {};
+    obj[key] = value;
+
+    if (buckets[index] !== undefined) {
+      // create new array with old values and new value added on
+      buckets[index] = Array.prototype.concat.call(buckets[index], obj);
+    } else {
+      // else, just put single object in bucket
+      buckets[index] = obj;
+    }
+
+    let maxCapacity = Math.floor(currentCapacity * 0.75);
+    let currentLength = length();
+
+    // if there is already something here, handle the collision
+    if (currentLength === maxCapacity) {
+      currentCapacity = currentCapacity * 2; // double the size of the old bucket list
+    }
   }
 
   function get(key) {
-    // write code here
+    let index = hash(key);
+    if (Array.isArray(buckets[index])) {
+      // if array, check all array values for the key
+      for (let i = 0; i < buckets[index].length; i++) {
+        if (Object.keys(buckets[index][i]).toString() === key) {
+          return Object.values(buckets[index][i]).toString();
+        }
+      }
+      // or else return null
+      return null;
+    } else {
+      // if not an array
+      if (buckets[index] !== undefined) {
+        return buckets[index][key];
+      } else {
+        return null;
+      }
+    }
   }
 
   function has(key) {
-    // write code here
+    let index = hash(key);
+    if (Array.isArray(buckets[index])) {
+      // if array, check all array values for the key
+      for (let i = 0; i < buckets[index].length; i++) {
+        if (Object.keys(buckets[index][i]).toString() === key) {
+          return true;
+        }
+      }
+      // or else return false
+      return false;
+    } else {
+      // if not an array
+      if (buckets[index] !== undefined) {
+        if (Object.keys(buckets[index]).toString() === key) {
+          return true;
+        }
+      } else {
+        return false;
+      }
+    }
   }
 
   function remove(key) {
-    // write code here
+    if (has(key) === true) {
+      let index = hash(key);
+      if (Array.isArray(buckets[index])) {
+        // if we splice all elements leaving only a single element array that should be an object
+        // if key is found, we set it equal to undefined
+        if (buckets[index].length === 1) {
+          if (Object.keys(buckets[index][0]).toString() === key) {
+            buckets[index] = undefined;
+            return true;
+          } else {
+            // else if object was not found then we return false
+            return false;
+          }
+        }
+        // if it is an array, check all array elements
+        for (let i = 0; i < buckets[index].length; i++) {
+          if (Object.keys(buckets[index][i]).toString() === key) {
+            buckets[index].splice(i, 1);
+            return true;
+          }
+        }
+        // else return false
+        return false;
+      } else {
+        // if not an array, set it equal to undefined
+        if (Object.keys(buckets[index]).toString() === key) {
+          return true;
+          buckets[index] = undefined;
+        }
+      }
+    } else {
+      return false;
+    }
   }
 
   function length() {
-    // write code here
+    let count = 0;
+    for (let i = 0; i < buckets.length; i++) {
+      if (buckets[i] !== undefined) {
+        if (Array.isArray(buckets[i])) {
+          count += buckets[i].length;
+        } else {
+          count += 1;
+        }
+      }
+    }
+    return count;
   }
 
   function clear() {
-    // write code here
+    buckets = [];
+    currentCapacity = 16;
+    buckets.length = currentCapacity;
   }
 
   function keys() {
-    // write code here
+    const keysArray = [];
+    for (let i = 0; i < buckets.length; i++) {
+      if (Array.isArray(buckets[i])) {
+        for (let j = 0; j < buckets[i].length; j++) {
+          keysArray.push(Object.keys(buckets[i][j]).toString());
+        }
+      } else {
+        if (buckets[i] !== undefined) {
+          keysArray.push(Object.keys(buckets[i]).toString());
+        }
+      }
+    }
+    return keysArray;
   }
 
   function values() {
-    // write code here
+    const valuesArray = [];
+    for (let i = 0; i < buckets.length; i++) {
+      if (Array.isArray(buckets[i])) {
+        for (let j = 0; j < buckets[i].length; j++) {
+          valuesArray.push(Object.values(buckets[i][j]).toString());
+        }
+      } else {
+        if (buckets[i] !== undefined) {
+          valuesArray.push(Object.values(buckets[i]).toString());
+        }
+      }
+    }
+    return valuesArray;
   }
 
   function entries() {
-    // write code here
+    const entriesArray = [];
+    for (let i = 0; i < buckets.length; i++) {
+      if (buckets[i] !== undefined) {
+        // if there's been collisions, print the whole array
+        if (Array.isArray(buckets[i])) {
+          entriesArray.push(buckets[i]);
+        } else {
+          entriesArray.push(
+            Object.keys(buckets[i]).concat(Object.values(buckets[i]))
+          );
+        }
+      }
+    }
+    return entriesArray;
   }
 
-  return { hash };
+  // just to check if total number of buckets we currently have grows properly
+  function returnCapacity() {
+    return currentCapacity;
+  }
+
+  return {
+    hash,
+    set,
+    get,
+    has,
+    remove,
+    length,
+    clear,
+    keys,
+    values,
+    entries,
+    returnCapacity,
+  };
 }
